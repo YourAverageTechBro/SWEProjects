bab7ea460397c0bcc7d694e0c5eb11e719b35c0c
YourAverageTechBro <94861246+YourAverageTechBro@users.noreply.github.com>
2023-05-03 15:53:23 -0500

    Remove debug logs (#13)

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,15 +6,5 @@ export default withClerkMiddleware(() => {
 });
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    "/((?!static|.*\\..*|_next|favicon.ico).*)",
-    "/",
-  ],
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
