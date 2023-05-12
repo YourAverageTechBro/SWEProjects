38fea520624d07b8e2c267f79b2b7afc5f6a27ea
Dohyun Kim <dohyun@youraveragetechbro.com>
2023-05-12 18:09:52 -0500

    Track conversions from backend

--- src/pages/projects/successfulPurchase.tsx
+++ src/pages/projects/successfulPurchase.tsx
@@ -1,32 +1,7 @@
-import { useRouter } from "next/router";
 import PersonCoding from "~/components/Images/PersonCoding";
-import { useEffect } from "react";
-import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
-import { usePostHog } from "posthog-js/react";
 
 export default function SuccessfulPurchase() {
-  const router = useRouter();
-  const { isSignedIn, user } = useUser();
-  const { projectId } = router.query as {
-    projectId: string;
-  };
-  const postHog = usePostHog();
-
-  useEffect(() => {
-    if (isSignedIn && user && projectId) {
-      postHog?.identify(user?.id, {
-        name: user?.fullName,
-        email: user?.primaryEmailAddress?.emailAddress,
-      });
-      postHog?.capture("Successfully Purchased Project", {
-        distinct_id: user.id,
-        project_id: projectId,
-        time: new Date(),
-      });
-    }
-  }, [isSignedIn, projectId, user]);
-
   return (
     <>
       <div
