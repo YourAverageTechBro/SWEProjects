04078abb9a771307a027d18634f553272ff2992c
Dohyun Kim <dohyun@youraveragetechbro.com>
2023-05-14 15:40:29 -0500

    Remove mailerlite changes

--- src/pages/api/webhooks/clerk/users.tsx
+++ src/pages/api/webhooks/clerk/users.tsx
@@ -1,4 +1,3 @@
-import clerk from "@clerk/clerk-sdk-node";
 import { Resend } from "resend";
 import { type IncomingHttpHeaders } from "http";
 import type { NextApiResponse } from "next";
@@ -6,7 +5,6 @@ import { Webhook, type WebhookRequiredHeaders } from "svix";
 import { buffer } from "micro";
 import WelcomeEmail from "emails/WelcomeEmail";
 import { type AxiomAPIRequest, withAxiom } from "next-axiom";
-import MailerLite from "@mailerlite/mailerlite-nodejs";
 
 // Disable the bodyParser, so we can access the raw
 // request body for verification.
@@ -24,10 +22,6 @@ const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const mailerlite = new MailerLite({
-  api_key: process.env.MAILER_LITE_API_KEY ?? "",
-});
-
 async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
@@ -44,10 +38,10 @@ async function handler(
     // Handle the webhook
     const eventType: EventType = evt.type;
     if (eventType === "user.created") {
-      const { id, email_addresses, first_name, last_name } = evt.data;
       req.log.info("[api/webhooks/clerk/users] Starting endpoint", {
         data: JSON.stringify(evt.data),
       });
+      const { id, email_addresses } = evt.data;
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       if (!id || !email_addresses || !email_addresses[0].email_address) {
         req.log.info("[api/webhooks/clerk/users] Invalid data from Clerk", {
@@ -64,8 +58,6 @@ async function handler(
         data: JSON.stringify({
           id: id as string,
           emailAddress,
-          first_name: first_name as string,
-          last_name: last_name as string,
         }),
       });
 
@@ -74,38 +66,6 @@ async function handler(
         to: emailAddress,
         subject: "Welcome to SWE Projects! Here are some helpful links",
         react: <WelcomeEmail />,
-      });
-
-      req.log.info("[api/webhooks/clerk/users] Adding user to mailer lite", {
-        data: JSON.stringify({
-          id: id as string,
-          emailAddress,
-          first_name: first_name as string,
-          last_name: last_name as string,
-        }),
-      });
-      const resp = await mailerlite.subscribers.createOrUpdate({
-        email: emailAddress,
-        fields: {
-          name: first_name as string,
-          last_name: last_name as string,
-        },
-        status: "active",
-        subscribed_at: new Date().toISOString(),
-      });
-
-      req.log.info("[api/webhooks/clerk/users] Updating clerk metadata", {
-        data: JSON.stringify({
-          id: id as string,
-          emailAddress,
-          first_name: first_name as string,
-          last_name: last_name as string,
-        }),
-      });
-      await clerk.users.updateUserMetadata(id as string, {
-        privateMetadata: {
-          mailerLiteId: resp.data.data.id,
-        },
       });
 
       req.log.info("[api/webhooks/clerk/users] Completed endpoint", {
