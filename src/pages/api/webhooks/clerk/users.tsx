aa8df91959ff21d23f9d0c919d997356f2ebd7ae
Dohyun Kim <dohyun@youraveragetechbro.com>
2023-05-14 13:22:44 -0500

    Make tutorial semi-mobile friendly

--- src/pages/api/webhooks/clerk/users.tsx
+++ src/pages/api/webhooks/clerk/users.tsx
@@ -1,9 +1,10 @@
 import { Resend } from "resend";
 import { type IncomingHttpHeaders } from "http";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiResponse } from "next";
 import { Webhook, type WebhookRequiredHeaders } from "svix";
 import { buffer } from "micro";
 import WelcomeEmail from "emails/WelcomeEmail";
+import { type AxiomAPIRequest, withAxiom } from "next-axiom";
 
 // Disable the bodyParser, so we can access the raw
 // request body for verification.
@@ -13,7 +14,7 @@ export const config = {
   },
 };
 
-type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+type NextApiRequestWithSvixRequiredHeaders = AxiomAPIRequest & {
   headers: IncomingHttpHeaders & WebhookRequiredHeaders;
 };
 
@@ -21,7 +22,7 @@ const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(
+async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
 ) {
@@ -34,10 +35,10 @@ export default async function handler(
   try {
     evt = wh.verify(payload, headers) as Event;
   } catch (e: any) {
-    console.error(
+    req.log.error(
       "[api/webhooks/clerk/users] Error",
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-      JSON.stringify({ error: e.message })
+      { error: e.message }
     );
     return res.status(400).json({});
   }
@@ -46,12 +47,12 @@ export default async function handler(
   const eventType: EventType = evt.type;
   if (eventType === "user.created") {
     const { id, email_addresses } = evt.data;
-    console.log("[api/webhooks/clerk/users] Starting endpoint", {
+    req.log.info("[api/webhooks/clerk/users] Starting endpoint", {
       data: JSON.stringify(evt.data),
     });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (!id || !email_addresses || !email_addresses[0].email_address) {
-      console.log("[api/webhooks/clerk/users] Invalid data from Clerk", {
+      req.log.info("[api/webhooks/clerk/users] Invalid data from Clerk", {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         data: JSON.stringify({ id, email_addresses }),
       });
@@ -64,12 +65,14 @@ export default async function handler(
       subject: "Welcome to SWE Projects! Here are some helpful links",
       react: <WelcomeEmail />,
     });
-    console.log("[api/webhooks/clerk/users] Completed endpoint", {
+    req.log.info("[api/webhooks/clerk/users] Completed endpoint", {
       data: JSON.stringify(evt.data),
     });
     res.json({});
   }
 }
+
+export default withAxiom(handler);
 
 // Generic (and naive) way for the Clerk event
 // payload type.
